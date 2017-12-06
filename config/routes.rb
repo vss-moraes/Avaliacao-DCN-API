9bf9dfbcdc1380c66e8e71898ac1f9b92dff3393
Vinicius Moraes <vsantana0212@gmail.com>
2017-12-06 20:27:01 -0300

    Corrigindo erro idiota

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
 
-  get '/avaliadors/:avaliador_id/notas', to: 'notas@index_avaliador'
+  get '/avaliadors/:avaliador_id/notas', to: 'notas#index_avaliador'
   get 'categoria/:categoria', to: 'inscritos#type'
 
   resources :inscritos do
