0091ad134e72ece4d9d5508389edc9305fcab66e
Vinicius Moraes <vsantana0212@gmail.com>
2017-12-07 09:23:53 -0300

    Alterando mensagem de erro para token inválido.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,12 @@ class ApplicationController < ActionController::API
   private
     def access_permission(perfil_types)
       @current_user = AuthorizeApiRequest.call(request.headers).result
-      unless perfil_types.include?(@current_user.perfil.class.name)
-        render json: {error: 'Not Authorized'}, status: 401
+      if @current_user
+        unless perfil_types.include?(@current_user.perfil.class.name)
+          render json: {error: 'Not Authorized'}, status: 401
+        end
+      else
+        render json: {error: 'Nenhum usuário encontrado'}, status: 401
       end
     end
 end
